c100d46953435d9eaad25254e03b2bfe0e794c8f
zero-section9 <zero.section9@gmail.com>
2013-08-14 17:33:28 +0530

    Completes Prog 5

--- prog5.c
+++ prog5.c
@@ -10,40 +10,47 @@
 int main(int argc, char *argv[]) {
     char buf[100];
     socklen_t len;
-    int k, sock_desc, temp_sock_desc;
+    int port, k, sock_desc, temp_sock_desc;
     struct sockaddr_in client, server;
-    memset(&client, 0, sizeof(client));
+    
     memset(&server, 0, sizeof(server));
+    if (argc == 2) {
+        port = atoi(argv[1]);
+    } else {
+        fprintf(stderr, "Usage: %s <port>\n");
+        return 1;
+    }
+    
     printf("Initializing Server... ");
     sock_desc = socket(AF_INET, SOCK_STREAM, 0);
     server.sin_family = AF_INET;
     server.sin_addr.s_addr = inet_addr("127.0.0.1");
-    server.sin_port = 37777;
+    server.sin_port = port;
+
     printf("done.\nBinding Server... ");
     k = bind(sock_desc, (struct sockaddr *) &server, sizeof(server));
     if (k < 0) {
-         perror(errno);
+         printf("\n");
+         perror("bind");
          return k;
     }
-    printf("done.\nStart Listening... ");
+    printf("done.\nStart Listening at port %d... ", port);
     k = listen(sock_desc, 20);
     if (k < 0) {
-         perror(errno);
+         printf("\n");
+         perror("listen");
          return k;
     }
     printf("done.\n");
-    len = sizeof(client);
     
-    int i = 0;
-    while (1) {
+    len = sizeof(client);
+    while (1) { 
+        memset(&client, 0 , sizeof(client));
         temp_sock_desc = accept(sock_desc, (struct sockaddr *) &client, &len);
-        printf("Connection %d:\n", i);
-        while (1) {
-            k = recv(temp_sock_desc, buf, 100, 0);
-            if (k > 0) {
-                printf("%s", buf);
-            }
-        }
+        printf("Received Connection from Address: %s\n", inet_ntoa(client.sin_addr));
+        close(temp_sock_desc);
     }
+    close(sock_desc);
+
     return 0;
 }
---
+++ prog5_client.c
@@ -0,0 +1,39 @@
+#include <sys/socket.h> 
+#include <netinet/in.h> 
+#include <stdio.h> 
+#include <string.h> 
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <errno.h>
+
+int main(int argc, char *argv[]) {
+    char buf[100];
+    struct sockaddr_in client;
+    int port, sock_desc, k;
+
+    if (argc == 2) {
+        port = atoi(argv[1]);
+    } else {
+        fprintf(stderr, "Usage: %s <port number>\n");
+        return 1;
+    }
+
+    sock_desc = socket(AF_INET, SOCK_STREAM, 0);
+    memset(&client, 0, sizeof(client));
+    client.sin_family = AF_INET;
+    client.sin_addr.s_addr = inet_addr("127.0.0.1");
+    client.sin_port = port;
+
+    printf("Establishing Connection... ");
+    k = connect(sock_desc, (struct sockaddr*)&client, sizeof(client));
+    if (k < 0) {
+        printf("\n");
+        perror("connect");
+        return k;
+    }
+    printf("tested successfully.\n");
+    close(sock_desc);
+
+    return 0;
+}
