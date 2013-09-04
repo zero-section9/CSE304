d78d716402911961e6d0e9b78401287b67330809
zero-section9 <zero.section9@gmail.com>
2013-09-04 17:26:46 +0530

    Adds prog10

---
+++ prog10.c
@@ -0,0 +1,43 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+    int skt, port, len, k;
+    struct sockaddr_in address, client;
+    char buf[1024];
+    const char success_res[] = "Message Received Successfully.";
+    const char err_res[] = "Transmision Error!";
+
+    if (argc == 2) {
+        port = atoi(argv[1]);
+    } else {
+        fprintf(stderr, "Usage: %s <port>\n", argv[0]);
+        return 1;
+    }
+
+    skt = socket(AF_INET, SOCK_DGRAM, 0);
+    
+    memset(&address, 0, sizeof(address));
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = htonl(INADDR_ANY);
+    address.sin_port = htons(port);
+
+    if (bind(skt, (struct sockaddr *) &address, sizeof(address)) < 0) {
+        perror("While binding");
+        return 0;
+    }
+    
+    len = sizeof(client);
+    memset(&client, 0 , sizeof(client));
+    if(recvfrom(skt, buf, sizeof(buf), 0, (struct sockaddr *) &client, &len) < 0) {
+        perror("While Receiving");
+    }
+    close(skt);
+
+    return 0;
+}
---
+++ prog10_client.c
@@ -0,0 +1,37 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+    int skt, port, len, k;
+    struct sockaddr_in server, srvres;
+    char buf[1024], res[1024];
+
+    if (argc == 2) {
+        port = atoi(argv[1]);
+    } else {
+        fprintf(stderr, "Usage: %s <port>\n", argv[0]);
+        return 1;
+    }
+
+    skt = socket(AF_INET, SOCK_DGRAM, 0);
+    
+    memset(&server, 0, sizeof(server));
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons(port);
+    
+    len = sizeof(srvres);
+    printf("> ");
+    scanf("%s", buf);
+    if(sendto(skt, buf, strlen(buf)+1, 0, (struct sockaddr *) &server, sizeof(server)) < 0) {
+        perror("While Sending");
+    }
+    close(skt);
+
+    return 0;
+}
